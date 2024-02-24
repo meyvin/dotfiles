451dd312dd9377ee4f109483ba0bfeafaad14e60
Meyvin <meyvin@serverbak.nl>
2024-02-24 21:10:48 +0100

    changes made for my thinkpad z16

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,15 +3,13 @@ set -g fish_greeting
 
 ## Env variables
 set -gx EDITOR "nvim"
+set -gx LANG "en_US.UTF-8"
+set -gx MOZ_USE_XINPUT2 "1"
 set -gx QT_QPA_PLATFORMTHEME "qt5ct"
 set -gx VISUAL "textadept"
 set -gx XDG_CURRENT_DESKTOP "i3"
 set -gx XDG_SESSION_DESKTOP "i3"
 set -gx XDG_SESSION_TYPE "x11"
-
-## Nvidia Firefox
-set -gx NVD_BACKEND "direct"
-set -gx MOZ_DISABLE_RDD_SANDBOX "1"
 
 ## Theming
 set -gx GTK_THEME "Adwaita:dark"
@@ -30,5 +28,6 @@ fish_add_path ~/.local/share/JetBrains/Toolbox/scripts
 alias fishconf="nvim ~/.config/fish/config.fish"
 alias i3conf="nvim ~/.config/i3/config"
 alias sail="vendor/bin/sail"
+alias sysyadm="sudo yadm --yadm-dir /etc/yadm --yadm-data /etc/yadm/data"
 alias vi="nvim"
 alias vim="nvim"
